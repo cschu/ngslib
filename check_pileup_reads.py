4c5293c243ee550cab2a5b5a1f7874c6993cd016
Christian Schudoma <spam@darkjade.net>
2012-08-21 11:47:45 +0200

    .

---
+++ check_pileup_reads.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+Created on Aug 21, 2012
+
+@author: Chris
+'''
+
+import sys
+
+import pysam
+
+
+def main(argv):
+    
+    samfile = pysam.Samfile(argv[0], 'rb')
+    
+    
+    for col in samfile.pileup('Chr1', start=4880, end=4881, stepper='all'):
+        
+        if col.pos == 4880:                                              
+            for read in col.pileups:
+                print read
+    
+    samfile.close()
+            
+    
+    
+    pass
+
+if __name__ == '__main__': main(sys.argv[1:])
