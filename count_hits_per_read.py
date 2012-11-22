c4f2209362652d4370de191e22df70a82cf9c7df
Christian Schudoma <spam@darkjade.net>
2012-11-22 12:02:28 +0100

    Added count_hits_per_read.py

---
+++ count_hits_per_read.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''
+Created on Nov 22, 2012
+
+@author: Christian Schudoma (schudoma@mpimp-golm.mpg.de, cschu@darkjade.net)
+'''
+import sys
+import pysam
+
+def main(argv):
+    bamfile = pysam.Samfile(argv[0], 'rb')
+    counts = {}
+    for alignedRead in bamfile.fetch():
+        counts[alignedRead.qname] = counts.get(alignedRead.qname, 0) + 1
+    bamfile.close()
+    
+    out = open(argv[0] + '.mult_counts', 'wb')
+    for item in sorted(counts.items()):
+        out.write('%s\t%i\n' % item)
+    out.close()
+    
+    
+    pass
+
+if __name__ == '__main__': main(sys.argv[1:])
